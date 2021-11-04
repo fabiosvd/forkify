78ce623cda20261641b748c3bdc151266f85f520
fabiosvd <fabiosalvador85@yahoo.com.br>
2021-11-04 19:42:18 -0300

    implementing search results - part 1

--- src/js/model.js
+++ src/js/model.js
@@ -3,11 +3,15 @@ import { API_URL } from './config.js';
 import { getJSON } from './helpers.js';
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     let { recipe } = data.data;
     state.recipe = {
@@ -21,6 +25,28 @@ export const loadRecipe = async function (id) {
       ingredients: recipe.ingredients,
     };
   } catch (err) {
+    console.log(`${err} 💥`);
     throw err;
   }
 };
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        sourceUrl: rec.source_url,
+        image: rec.image_url,
+      };
+    });
+  } catch (err) {
+    console.log(`${err} 💥`);
+    throw err;
+  }
+};
+loadSearchResults('pizza');
